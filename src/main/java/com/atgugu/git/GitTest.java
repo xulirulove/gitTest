4d5bf568f6f0ea43d679c3c343243dbd90377f02
xiaoxu <xiaoxu@qq.com>
2022-10-21 00:02:15 +0800

    push commit

--- src/main/java/com/atgugu/git/GitTest.java
+++ src/main/java/com/atgugu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello  ");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
